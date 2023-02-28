aa491ef434fd23313c333ffb5bae188fa9d4ffc6
Anunay Kumar <annunay711998@gmail.com>
2023-02-28 23:52:27 +0530

    Commit changes to my code

--- eFoodHub.WebUI/Models/ItemModel.cs
+++ eFoodHub.WebUI/Models/ItemModel.cs
@@ -11,21 +11,36 @@ namespace eFoodHub.WebUI.Models
     {
         public int Id { get; set; }
 
+
         [Required(ErrorMessage = "Please Enter Name")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Please enter the valid Name")]
+
         public string Name { get; set; }
+
         public IFormFile File { get; set; }
 
+
         [Required(ErrorMessage = "Please Enter Description")]
+
         public string Description { get; set; }
 
+
         [Required(ErrorMessage = "Please Enter UnitPrice")]
+        [RegularExpression(@"^(?!(?:0|0\.0|0\.00)$)[+]?\d+(\.\d|\.\d[0-9])?$", ErrorMessage = "Please enter valid price")]
+
         public decimal UnitPrice { get; set; }
+
         public string ImageUrl { get; set; }
 
+
         [Required(ErrorMessage = "Please Select Category")]
+
         public int CategoryId { get; set; }
 
+
         [Required(ErrorMessage = "Please Select Item")]
+
         public int ItemTypeId { get; set; }
+
     }
 }
--- eFoodHub.WebUI/Models/UserModel.cs
+++ eFoodHub.WebUI/Models/UserModel.cs
@@ -10,19 +10,38 @@ namespace eFoodHub.WebUI.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Please Enter Email")]
-        public string Email { get; set; }
 
+        [Required(ErrorMessage = "Please Enter Email")]
+
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please provide valid email format")]
+
+        public string Email { get; set; }
         [Required(ErrorMessage = "Please Enter Name")]
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Please enter only characters")]
+
+
+
         public string Name { get; set; }
 
+
         [Required(ErrorMessage = "Please Enter Password")]
+
+        [RegularExpression("^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!#$%&?_@\"]).*$", ErrorMessage = "Password should be atleast Eight combination of A-Z,a-z,0-9,!_@#$%^&*")]
+
         public string Password { get; set; }
 
+
         [Compare("Password", ErrorMessage = "Compare Password doesn't match")]
+
         public string ConfirmPassword { get; set; }
 
+
         [Required(ErrorMessage = "Please Enter Phone Number")]
+
+        [RegularExpression("^(?:(?:\\+|0{0,2})91(\\s*[\\ -]\\s*)?|[0]?)?[789]\\d{9}|(\\d[ -]?){10}\\d$", ErrorMessage = "Please enter valid phone number")]
+
         public string PhoneNumber { get; set; }
+
     }
 }
